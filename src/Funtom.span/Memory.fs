f7e84b21160758adceecb6decc20ae1b875eb86b
midoliy <midoliy@outlook.jp>
2023-02-13 23:25:11 +0900

    v0.0.1

---
+++ src/Funtom.span/Memory.fs
@@ -0,0 +1,14 @@
+﻿namespace Funtom.span
+
+#nowarn "9"
+
+open System
+open System.Runtime.InteropServices
+open FSharp.NativeInterop
+
+module Memory =
+  let inline stackalloca<'a when 'a: unmanaged> size : Span<'a> =
+    let p = NativePtr.stackalloc<'a> size |> NativePtr.toVoidPtr
+    Span<'a>(p, size)
+
+  let inline ref (x: Span<'a>) = MemoryMarshal.GetReference x
---
+++ src/Funtom.span/SpanExtensions.fs
@@ -0,0 +1,13 @@
+﻿namespace Funtom.span
+
+open System
+
+[<AutoOpen>]
+module Span =
+  type ReadOnlySpan<'T> with
+    member __.GetSlice(start_idx, end_idx) =
+      __.Slice(start_idx, end_idx)
+
+  type Span<'T> with
+    member __.GetSlice(start_idx, end_idx) =
+      __.Slice(start_idx, end_idx)
