41067603f2debd767277c7510f44cb2a7ec518c3
midoliy <midoliy@outlook.jp>
2023-03-11 00:13:07 +0900

    update

--- src/Funtom.span/Memory.fs
+++ src/Funtom.span/Memory.fs
@@ -6,12 +6,19 @@ open System
 open System.Runtime.InteropServices
 open FSharp.NativeInterop
 
-module Memory =
-  let inline stackalloc<'a when 'a: unmanaged> size : nativeptr<'a> =
-    NativePtr.stackalloc<'a> size
+[<Measure>] type byte
 
-  let inline stackalloca<'a when 'a: unmanaged> size : Span<'a> =
-    let p = NativePtr.stackalloc<'a> size |> NativePtr.toVoidPtr
-    Span<'a>(p, size)
+module Memory =
+  let mutable threshold = 128<byte>
+
+  let inline stackalloc<'a when 'a: unmanaged> (size: int<byte>) : nativeptr<'a> =
+    NativePtr.stackalloc<'a> (int size)
+
+  let inline stackalloca<'a when 'a: unmanaged> (size: int<byte>) : Span<'a> =
+    let p = NativePtr.stackalloc<'a> (int size) |> NativePtr.toVoidPtr
+    Span<'a>(p, (int size))
+
+  let inline alloc<'a when 'a: unmanaged> (size: int<byte>) = 
+    if size <= threshold then stackalloca size else Array.zeroCreate<'a>(int size).AsSpan()
 
   let inline ref (span: Span<'a>) = &(MemoryMarshal.GetReference span)
