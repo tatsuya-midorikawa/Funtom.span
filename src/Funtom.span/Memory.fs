e468000f950c63e51c584650fb23060ac3a764b2
midoliy <midoliy@outlook.jp>
2023-03-11 00:36:26 +0900

    update

--- src/Funtom.span/Memory.fs
+++ src/Funtom.span/Memory.fs
@@ -3,22 +3,27 @@
 #nowarn "9"
 
 open System
+open System.Buffers
 open System.Runtime.InteropServices
 open FSharp.NativeInterop
 
-[<Measure>] type byte
-
 module Memory =
-  let mutable threshold = 128<byte>
+  let mutable threshold = 128
 
-  let inline stackalloc<'a when 'a: unmanaged> (size: int<byte>) : nativeptr<'a> =
-    NativePtr.stackalloc<'a> (int size)
+  let inline stackalloc<'a when 'a: unmanaged> (size: int) : nativeptr<'a> =
+    NativePtr.stackalloc<'a> size
 
-  let inline stackalloca<'a when 'a: unmanaged> (size: int<byte>) : Span<'a> =
+  let inline stackalloca<'a when 'a: unmanaged> (size: int) : Span<'a> =
     let p = NativePtr.stackalloc<'a> (int size) |> NativePtr.toVoidPtr
-    Span<'a>(p, (int size))
+    Span<'a>(p, size)
 
-  let inline alloc<'a when 'a: unmanaged> (size: int<byte>) = 
-    if size <= threshold then stackalloca size else Array.zeroCreate<'a>(int size).AsSpan()
+  let inline alloc<'a when 'a: unmanaged> (size: int) = 
+    if size <= threshold then stackalloca size else Array.zeroCreate<'a>(size).AsSpan()
+    
+  let inline rent<'a when 'a: unmanaged> (size: int) =
+    ArrayPool<'a>.Shared.Rent size
+    
+  let inline returns<'a when 'a: unmanaged> (ary: array<'a>) =
+    ArrayPool<'a>.Shared.Return ary
 
   let inline ref (span: Span<'a>) = &(MemoryMarshal.GetReference span)
